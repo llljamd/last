50427cf75fbb2377e8a561cbdd10494cd0e9cb3c
Alex Nguyen <nguyeal2@oregonstate.edu>
2021-03-12 16:49:23 -0800

    added more multiply tests

--- test_calculator.py
+++ test_calculator.py
@@ -11,3 +11,9 @@ class Test:
 
     def test_3(self):
         assert 4 == calculator.multiply(2, 2)
+
+    def test_4(self):
+        assert 9 == calculator.multiply(3, 3)
+
+    def test_5(self):
+        assert 0 == calculator.multiply(209, 0)
