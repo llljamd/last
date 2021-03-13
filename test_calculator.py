5bf566a89f591e3fb4350ba2b61be43b639b7cd2
Alex Nguyen <nguyeal2@oregonstate.edu>
2021-03-12 16:42:42 -0800

    fixed files

--- test_calculator.py
+++ test_calculator.py
@@ -3,11 +3,11 @@ import calculator
 
 class Test:
 
-	def test_1(self):
-		assert 10 == calculator.add(6, 4)
+    def test_1(self):
+        assert 10 == calculator.add(6, 4)
 
-	def test_2(self):
-		assert 5 == calculator.subtract(8, 3)
+    def test_2(self):
+        assert 5 == calculator.subtract(8, 3)
 
-	def test_3(self):
-		assert 4 == calculator.multiply(2, 2)
+    def test_3(self):
+        assert 4 == calculator.multiply(2, 2)
